f31131aad7cae131e0bd3c2d85617f1f70baa053
Ruslan Zinnatov <rh26157@gmail.com>
2022-10-24 17:14:03 +0300

    complete fourth step

--- brain_games_package/scripts/brain_games.py
+++ brain_games_package/scripts/brain_games.py
@@ -1,9 +1,8 @@
 #!usr/bin/env python
-from brain_games.cli import welcome_user
+from brain_games_package.cli import welcome_user
 
 
 def main():
-    print("Welcome to the Brain Games!")
     welcome_user()
 
 
