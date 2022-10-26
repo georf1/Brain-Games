ffd1d3e35d852a3d42f5b1208a9870dcac4f6d67
Ruslan Zinnatov <rh26157@gmail.com>
2022-10-26 20:58:03 +0300

    add brain_progression game

---
+++ brain_games_package/games/progression.py
@@ -0,0 +1,22 @@
+from random import randint
+
+
+description = "What number is missing in the progression?"
+
+
+def start_round():
+    condition = ''
+
+    length = randint(5, 10)
+    lst = list(range(randint(1, 25), randint(80, 100), randint(1, 10)))[:length]
+
+    random_index = randint(0, len(lst) - 1)
+    right_answer = lst[random_index]
+
+    lst.pop(random_index)
+    lst.insert(random_index, '..')
+
+    for el in lst:
+        condition += str(el) + ' '
+
+    return condition.strip(), str(right_answer)
---
+++ brain_games_package/scripts/brain_progression.py
@@ -0,0 +1,10 @@
+from brain_games_package.games_logic import start_game
+from brain_games_package.games import progression
+
+
+def main():
+    start_game(progression)
+
+
+if __name__ == '__main__':
+    main()
