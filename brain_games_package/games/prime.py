59360b9b250e7b4cdae07f848682f92f6111d267
Ruslan Zinnatov <rh26157@gmail.com>
2022-10-26 22:09:17 +0300

    change prime game description

--- brain_games_package/games/prime.py
+++ brain_games_package/games/prime.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-DESCRIPTION = 'Answer "yes" if the number is prime, otherwise answer "no".'
+DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def start_round():
