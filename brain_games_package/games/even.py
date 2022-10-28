e9e15894e10ff03b5fe9540f4a8f1bb0e3089975
Ruslan Zinnatov <rh26157@gmail.com>
2022-10-28 16:13:47 +0300

    update even.py

--- brain_games_package/games/even.py
+++ brain_games_package/games/even.py
@@ -4,8 +4,12 @@ from random import randint
 DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
+def isEven(num):
+    return num % 2 == 0
+
+
 def start_round():
     condition = randint(1, 100)
-    right_answer = condition % 2 == 0 and 'yes' or 'no'
+    right_answer = isEven(condition) and 'yes' or 'no'
 
     return condition, right_answer
