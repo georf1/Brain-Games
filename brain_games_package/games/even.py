984bc27020fdf9eddbe546b8526fcb3cf7df836e
Ruslan Zinnatov <rh26157@gmail.com>
2022-10-30 14:44:07 +0300

    update even.py and prime.py

--- brain_games_package/games/even.py
+++ brain_games_package/games/even.py
@@ -4,12 +4,12 @@ from random import randint
 DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def isEven(num):
+def is_even(num):
     return num % 2 == 0
 
 
 def generate_round():
     num = randint(1, 100)
-    right_answer = isEven(num) and 'yes' or 'no'
+    right_answer = 'yes' if is_even(num) else 'no'
 
     return num, right_answer
--- brain_games_package/games/prime.py
+++ brain_games_package/games/prime.py
@@ -4,20 +4,20 @@ from random import randint
 DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def isPrime(num):
+def is_prime(num):
     divider = 2
 
     while divider * divider <= num and num % divider != 0:
         divider += 1
 
     if num >= 2 and divider * divider > num:
-        return 'yes'
+        return True
     else:
-        return 'no'
+        return False
 
 
 def generate_round():
     num = randint(1, 100)
-    right_answer = isPrime(num)
+    right_answer = 'yes' if isPrime(num) else 'no'
 
     return num, right_answer
